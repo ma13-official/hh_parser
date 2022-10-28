2f9a297610a7ccd93be3c7ce726847f7f9602e0b
ma13-official <ma13.official.ru@gmail.com>
2022-10-29 00:27:30 +0300

    get_specializations added

--- main.py
+++ main.py
@@ -1,32 +1,76 @@
+import operator
+
 import requests
 from openpyxl import Workbook
 
 
-class api_hh_connect:
+class APIHHConnect:
     def __init__(self):
+        self.specializations = None
         self.vacancies = None
         self.vacancies_array = []
         self.cur_number = 0
 
     def checking_all_pages(self):
         for x in range(0, 20, 1):
-            self.creating_connection(x)
+            self.get_vacancies(x)
             self.filling_vacancies_array(self.vacancies)
 
-    def creating_connection(self, page):
+    def get_vacancies(self, page):
         url = "https://api.hh.ru/vacancies"
         params = {'per_page': 100, 'page': page}
         self.vacancies = requests.get(url, params).json()
+
+    """
+        JSON по ссылке "https://api.hh.ru/specializations" имеет следующую структуру:
+            - массив словарей(один словарь - одна область специализаций);
+            - каждый такой словарь имеет три поля:
+                - id:
+                - name;
+                - specializations (массив специализаций).
+        Реализованная функция:
+            - делает запрос, получает JSON;
+            - парсит JSON в массив с подобной структурой;
+            - сортирует массив по id областей специализаций и массив специализаций по их id;
+            - выводит полученный результат.
+        """
+    def get_specializations(self):
+        url = "https://api.hh.ru/specializations"
+        self.specializations = requests.get(url).json()
+        specializations_parsed = []
+        for group in self.specializations:
+            specs = {"id": int(group["id"]),
+                     "name": group["name"],
+                     "specializations": []}
+            for spec in group["specializations"]:
+                specs["specializations"].append({"id": spec["id"],
+                                                 "name": spec["name"]})
+            specs["specializations"] = sorted(specs["specializations"], key=self.get_data)
+            specializations_parsed.append(specs)
+        specializations_parsed = sorted(specializations_parsed, key=operator.itemgetter("id")).copy()
+        for group in specializations_parsed:
+            print("----------------------------------------")
+            print(group["id"], group["name"])
+            print("----------------------------------------")
+            for item in group["specializations"]:
+                print("    ", item["id"], item["name"])
+        # print(json.dumps(self.specializations, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': ')))
+
+    @staticmethod
+    def get_data(x):
+        x = x["id"]
+        for i in x:
+            x = x[1:]
+            if i == ".":
+                break
+        return int(x)
 
     def filling_vacancies_array(self, vacancies):
         for vacancy in vacancies[list(vacancies.keys())[0]]:
             self.cur_number += 1
             cur_vacancy = [self.cur_number]
-            try:
-                for field in list(vacancy.keys()):
-                    cur_vacancy.append(self.check_inner_fields(vacancy, field))
-            except:
-                print(vacancies.keys())
+            for field in list(vacancy.keys()):
+                cur_vacancy.append(self.check_inner_fields(vacancy, field))
             self.vacancies_array.append(cur_vacancy)
 
     @staticmethod
@@ -34,9 +78,9 @@ class api_hh_connect:
         if vacancy[field] is not None:
             if isinstance(vacancy[field], dict):
                 if len(vacancy[field]) == 2 or field == 'area' or field == 'employer':
-                    try:
+                    if field != 'snippet':
                         return vacancy[field]['name']
-                    except:
+                    else:
                         return None
             if isinstance(vacancy[field], list):
                 if not vacancy[field]:
@@ -65,4 +109,4 @@ class api_hh_connect:
         self.write_in_xlsx()
 
 
-api_hh_connect().start()
+APIHHConnect().start()
